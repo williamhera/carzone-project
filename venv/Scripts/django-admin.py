2e5c6d77e55cc22be5033282d00079c3e61e8eff
William Hera <whera2012@gmail.com>
2021-05-28 17:08:31 +1000

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\william hera\documents\01_personal development\lectures\rathan kumar - django\carzone_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
